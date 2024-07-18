d0811fea65355dd29ed8b09c02b0c0bad08c0ba1
Charif Chatreewong <master-charif@outlook.com>
2024-07-18 18:47:15 +0700

    fix map
    fix map in contact page

--- script-contactPage.js
+++
@@ -1,7 +0,0 @@
-function map() {
-    var mapProp = {
-        center: new google.maps.LatLng(9.367961, 99.439242),
-        zoom: 11,
-    };
-    var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
-}
--- test.js
+++
@@ -1,4 +0,0 @@
-import { products } from "/data/data.js"
-console.log(products)
-const index = products.find(Obj => Obj.id === 25)
-console.log(index === undefined);
